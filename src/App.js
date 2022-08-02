0f0b652053d74b774d8b2f8c6861a034bb395aed
jung0930 <wjddlqkqh57@naver.com>
2022-08-03 00:05:16 +0900

    Feat: React에서 데이터 추가하기 단방향데이터 흐름, 역방향이벤트 흐름을 파악하고 state 끌어올리기를 이용하여 컴포넌트 추가해보기

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
-import "./App.css";
+import React, { useState, useRef } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-
-const dummyList = [
-  {
-    id: 1,
-    author: "임정",
-    content: "하이1",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "홍길동",
-    content: "하이2",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "이순신",
-    content: "하이3",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-];
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
