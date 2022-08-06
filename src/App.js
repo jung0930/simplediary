1905e2c3143cc4ea9f0904cd70286dee17efd373
jung0930 <wjddlqkqh57@naver.com>
2022-08-07 00:51:33 +0900

    Feat: 컴포넌트 재사용 (React.memo)

--- src/App.js
+++ src/App.js
@@ -44,7 +44,6 @@ function App() {
   };
 
   const onRemove = (targetId) => {
-    console.log(`${targetId}가 삭제되었습니다.`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   };
@@ -58,8 +57,6 @@ function App() {
   };
 
   const getDiaryAnalysis = useMemo(() => {
-    console.log("일기 분석 시작");
-
     const goodCount = data.filter((it) => it.emotion >= 3).length;
     const badCount = data.length - goodCount;
     const goodRatio = (goodCount / data.length) * 100;
