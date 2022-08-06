a1f7267fb686befb448e87bfde3cda2edc768dd2
jung0930 <wjddlqkqh57@naver.com>
2022-08-06 23:47:34 +0900

    Feat: 연산 결과 재사용(Memoization) 현재 일기 데이터를 분석하는 함수를 제작하고 해당 함수가 일기 데이터의 길이가 변화하지 않을 때 값을 다시 계산하지 않도록 하기

--- src/Reference/Lifecycle.js
+++ src/Reference/Lifecycle.js
@@ -1,17 +1,44 @@
 import React, { useState, useEffect } from "react";
 
 const UnmountTest = () => {
+  // Unmount 시점에 실행하게 됨
   useEffect(() => {
     console.log("Mount!");
-
     return () => {
-      // Unmount 시점에 실행하게 됨
       console.log("Unmount!");
     };
   }, []);
 
   return <div>Unmount Testing Component</div>;
 };
+
+// Mount 되는 시점에 실행
+// useEffect(() => {
+//   console.log("Mount!");
+// }, []);
+
+// 컴포넌트가 리렌더(업데이트) 되는 시점에 실행
+// useEffect(() => {
+//   console.log("Mount!");
+//   return () => {
+//     console.log("Unmount!");
+//   };
+// });
+
+// count status가 변화하는 순간 실행
+// useEffect(() => {
+//   console.log("Mount!");
+//   return () => {
+//     console.log("Unmount!");
+//   };
+// }, [count]);
+
+// Unmount 되는 시점에 실행
+// useEffect(() => {
+//  return () => {
+//    console.log("Unmount!");
+//  };
+// }, []);
 
 const LifeCycle = () => {
   const [isVisible, setIsVisible] = useState(false);
